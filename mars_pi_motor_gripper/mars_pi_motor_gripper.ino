47593f82b8ac73adca084993a46fec05a07c2903
sipimars <jonathan.west@bie.edu>
2016-02-23 17:33:25 -0700

    Update mars_pi_motor_gripper.ino
    Added some comments

--- mars_pi_motor_gripper/mars_pi_motor_gripper.ino
+++ mars_pi_motor_gripper/mars_pi_motor_gripper.ino
@@ -1,21 +1,32 @@
-
+/******************************************************
+ * Arduino code for the roadrunner with a motor shield for
+ * gearmotors and a servo powered gripped with a wrist joint
+ * ****************************************************/
 #include <ros.h>
 #include <geometry_msgs/Point.h>
 #include <Servo.h> 
  
-Servo grip;  // create servo object to control a servo 
-Servo wrist;  // create servo object to control a servo 
+// create servo objects to controls servos 
+Servo grip;  // open and close the gripper
+Servo wrist; // move wrist up and down
 
+// handle for the ros node interface
 ros::NodeHandle  nh;
 
+// map the pins to the motor shield functions
 int dir[] = {12,13};
 int pwm[] = {3, 11};
 int brake[] = {9, 8};
 int cur[] = {0,1};
+// set the directions for the left and right wheels
 int DIR_FWD[] = {LOW,HIGH};
 int DIR_REV[] = {HIGH,LOW};
+// loopCount is used as a timer to allow commands to act over a limited time
+// this is necessary in case communication is lost, the motors must 
+// automatically stop after some time
 int loopCount = 0;
 
+// map the servo motor pins
 #define WRIST_PIN 5
 #define GRIP_PIN  4
 
@@ -27,6 +38,7 @@ void stopMotors(void)
     }
 }
 
+// callback function when a motor command ROS message is received
 void motorMessageCb( const geometry_msgs::Point& cmd_msg){
   if( (cmd_msg.x == 0) && (cmd_msg.y == 0) ) {
     stopMotors();
@@ -49,11 +61,15 @@ void motorMessageCb( const geometry_msgs::Point& cmd_msg){
   return;
 }
 
+// callback function when a gripper command ROS message is received
 void gripMessageCb( const geometry_msgs::Point& cmd_msg){
+  // write the values directly to the servos.
+  // TODO add some limit checking or value mapping
   grip.write(cmd_msg.x);
   wrist.write(cmd_msg.y);
 }
 
+// create the ros topic subscribers
 ros::Subscriber<geometry_msgs::Point> motorSubscriber("motor_cmd", motorMessageCb );
 ros::Subscriber<geometry_msgs::Point> gripSubscriber("grip_cmd", gripMessageCb );
 
